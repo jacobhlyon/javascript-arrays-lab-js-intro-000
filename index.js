2a09a79525dd32f7d7c251c8c3937c9cf1ecd566
Jacob Harrison Lyon <jacobhlyon@gmail.com>
2017-08-07 16:06:11 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name){
+	kittens.push(name);
+	return kittens
+}
+
+function destructivelyPrependKitten(name){
+	kittens.unshift(name);
+	return kittens
+}
+
+function destructivelyRemoveLastKitten(){
+	kittens.pop();
+	return kittens
+}
+
+function destructivelyRemoveFirstKitten(){
+	kittens.shift();
+	return kittens
+}
+
+function appendKitten(name){
+	let newArray = [...kittens, name];
+	return newArray
+}
+
+function prependKitten(name){
+	let newArray = [name, ...kittens];
+	return newArray
+}
+
+function removeLastKitten(){
+	let newArray = kittens.slice(0, kittens.length-1);
+	return newArray
+}
+
+function removeFirstKitten(){
+	let newArray = kittens.slice(1);
+	return newArray
+}
